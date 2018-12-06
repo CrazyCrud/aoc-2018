7763fce91ff998907880a334efdb881dfcc94d16
Constantin Lehenmeier <constantin.lehenmeier@ur.de>
2018-12-06 16:47:02 +0100

    Startet bonus task of day 2

--- 02/main.py
+++ 02/main.py
@@ -15,19 +15,46 @@ def count_characters(word):
         elif value >= 3 and return_groups_of_three == 0:
             return_groups_of_three = 1
 
-    return return_groups_of_two, return_groups_of_three
+    char_counts['word'] = word
+
+    return return_groups_of_two, return_groups_of_three, char_counts
+
+
+def are_words_common(word_1, word_2):
+    assert len(word_1['word']) == len(word_2['word'])
+
+    word_1_chars = list(word_1.keys())
+    word_2_chars = list(word_2.keys())
+    common_keys = set(word_1_chars) - (set(word_1_chars) - set(word_2_chars))
+
+    if len(common_keys) == (len(word_1_chars) - 1):
+        return True
+    else:
+        return False
 
 
 if __name__ == "__main__":
+    words_chars = []
+
     with open('input.txt', 'r', encoding='utf-8') as input:
         groups_of_two_count = 0
         groups_of_three_count = 0
         for line in input:
             line = line.strip()
-            groups_of_two, groups_of_three = count_characters(line)
+            groups_of_two, groups_of_three, char_counts = count_characters(line)
+
             groups_of_two_count += groups_of_two
             groups_of_three_count += groups_of_three
+
+            words_chars.append(char_counts)
 
         checksum = groups_of_two_count * groups_of_three_count
 
         print("The checksum is {}".format(checksum))
+
+    for index_1, word_char_1 in enumerate(words_chars):
+        for index_2, word_char_2 in enumerate(words_chars):
+            if index_1 == index_2:
+                continue
+            if are_words_common(word_char_1, word_char_2):
+                pass
