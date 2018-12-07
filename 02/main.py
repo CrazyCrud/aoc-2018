1703541dadace662827c71a8bf1e4add29e62550
Constantin Lehenmeier <constantin.lehenmeier@ur.de>
2018-12-07 15:01:29 +0100

    Finished day 2

--- 02/main.py
+++ 02/main.py
@@ -15,46 +15,55 @@ def count_characters(word):
         elif value >= 3 and return_groups_of_three == 0:
             return_groups_of_three = 1
 
-    char_counts['word'] = word
+    return return_groups_of_two, return_groups_of_three
 
-    return return_groups_of_two, return_groups_of_three, char_counts
+
+def part_1(input_data):
+    groups_of_two_count = 0
+    groups_of_three_count = 0
+    for line in input_data:
+        line = line.strip()
+        groups_of_two, groups_of_three = count_characters(line)
+
+        groups_of_two_count += groups_of_two
+        groups_of_three_count += groups_of_three
+
+    checksum = groups_of_two_count * groups_of_three_count
+
+    print("The checksum is {}".format(checksum))
 
 
 def are_words_common(word_1, word_2):
-    assert len(word_1['word']) == len(word_2['word'])
+    assert len(word_1) == len(word_2)
 
-    word_1_chars = list(word_1.keys())
-    word_2_chars = list(word_2.keys())
-    common_keys = set(word_1_chars) - (set(word_1_chars) - set(word_2_chars))
+    equality = []
+    for i in range(len(word_1)):
+        equality.append(word_1[i] == word_2[i])
 
-    if len(common_keys) == (len(word_1_chars) - 1):
-        return True
-    else:
-        return False
+    return equality
+
+
+def get_resulting_word(word, equality):
+    return "".join([word[i] for i in range(len(word)) if equality[i]])
+
+
+def part_2(input_data):
+    for i in range(len(input_data)):
+        for j in range(i, len(input_data)):
+            word_1, word_2 = input_data[i], input_data[j]
+            if word_1 == word_2:
+                continue
+            equality = are_words_common(word_1, word_2)
+            if len([common_character for common_character in equality if not common_character]) == 1:
+                print("Words with one different character are {} and {}".format(word_1, word_2))
+                print("Without the differing character it is {}".format(get_resulting_word(word_1, equality)))
 
 
 if __name__ == "__main__":
-    words_chars = []
+    input_file = open('input.txt', 'r', encoding='utf-8')
+    lines = input_file.read().split("\n")
+    input_file.close()
 
-    with open('input.txt', 'r', encoding='utf-8') as input:
-        groups_of_two_count = 0
-        groups_of_three_count = 0
-        for line in input:
-            line = line.strip()
-            groups_of_two, groups_of_three, char_counts = count_characters(line)
+    part_1(lines)
 
-            groups_of_two_count += groups_of_two
-            groups_of_three_count += groups_of_three
-
-            words_chars.append(char_counts)
-
-        checksum = groups_of_two_count * groups_of_three_count
-
-        print("The checksum is {}".format(checksum))
-
-    for index_1, word_char_1 in enumerate(words_chars):
-        for index_2, word_char_2 in enumerate(words_chars):
-            if index_1 == index_2:
-                continue
-            if are_words_common(word_char_1, word_char_2):
-                pass
+    part_2(lines)
