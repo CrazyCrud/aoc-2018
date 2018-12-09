1c695c43dc97abd20032f9f9ac2473f941554972
Constantin Lehenmeier <info@constantinlehenmeier.de>
2018-12-09 23:01:28 +0100

    Created fabric

--- 03/main.py
+++ 03/main.py
@@ -1,4 +1,5 @@
 import re
+import numpy as np
 
 
 def read_input(input_data):
@@ -23,13 +24,40 @@ def read_input(input_data):
 
 
 def part_1(claims):
-    pass
+    fabric = np.array([[[[]] for j in range(1000)] for i in range(1000)])
+
+    print(fabric.shape)
+
+    for claim in claims:
+        x_offset = claim['x_offset']
+        y_offset = claim['y_offset']
+        id = claim['claim_id']
+        width = claim['width']
+        height = claim['height']
+
+        # print(id, x_offset, y_offset, width, height)
+
+        for x in range(x_offset, x_offset + width):
+            for y in range(y_offset, y_offset + height):
+                fabric_field = fabric[x][y]
+                fabric_field = np.append(fabric_field, id)
+                fabric[x][y] = fabric_field
+
+    number_of_claimed_fields = 0
+    for x in range(fabric.shape[0]):
+        for y in range(fabric.shape[1]):
+            if fabric[x, y].size > 1:
+                number_of_claimed_fields += number_of_claimed_fields
+
+    print("{} files are claimed by two or more ids".format(number_of_claimed_fields))
 
 
 if __name__ == "__main__":
     file = open('input.txt', 'r', encoding='utf-8')
 
     lines = file.read().split('\n')
+
+    file.close()
 
     claims = read_input(lines)
 
