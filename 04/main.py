971e0d628f7b3d85a86a49ff4b474c0dc38ccf8d
Constantin Lehenmeier <constantin.lehenmeier@ur.de>
2018-12-12 13:58:23 +0100

    Wrong solution on Day 4...

--- 04/main.py
+++ 04/main.py
@@ -2,35 +2,70 @@ import re
 import read_input
 
 
-def sort_input(input_data):
-    reg_ex = r'([0-9]{4})-([0-9]{2})-([0-9]{2}) ([0-9]{2}):([0-9]{2})'
+def get_minutes_of_each_guard(input_data):
+    regex_begin = r'\[([0-9]{4})-([0-9]{2})-([0-9]{2}) ([0-9]{2}):([0-9]{2})\] Guard #([0-9]+) begins shift'
+    regex_sleep = r'\[([0-9]{4})-([0-9]{2})-([0-9]{2}) ([0-9]{2}):([0-9]{2})\] falls asleep'
+    regex_awake = r'\[([0-9]{4})-([0-9]{2})-([0-9]{2}) ([0-9]{2}):([0-9]{2})\] wakes up'
 
-    input_data_sorted = []
+    return_minutes_of_each_guard = {}
+    return_minutes_data = [[] for x in range(60)]
 
-    records = []
+    guard_current = None
+
+    minute_start_of_sleep = None
+    minute_end_of_sleep = None
 
     for line in input_data:
-
-        match = re.match(reg_ex, line)
+        print(line)
+        match = re.match(regex_begin, line)
 
         if match:
-            record = {
-                'year': int(match.group(1)),
-                'month': int(match.group(2)),
-                'day': int(match.group(3)),
-                'hours': int(match.group(4)),
-                'minutes': int(match.group(5))
-            }
+            guard_current = (int)(match.group(6))
+            continue
 
-            print(record)
+        match = re.match(regex_sleep, line)
+        if match:
+            minute_start_of_sleep = (int)(match.group(5))
+            continue
 
-            records.append(record)
+        match = re.match(regex_awake, line)
+        if match:
+            minute_end_of_sleep = (int)(match.group(5))
+            sleep_duration = minute_end_of_sleep - minute_start_of_sleep
 
-    return input_data_sorted
+            if guard_current in return_minutes_of_each_guard:
+                return_minutes_of_each_guard[guard_current].append(sleep_duration)
+            else:
+                return_minutes_of_each_guard[guard_current] = [sleep_duration]
+
+            for i in range(minute_start_of_sleep, minute_end_of_sleep):
+                return_minutes_data[i].append(guard_current)
+
+    return return_minutes_of_each_guard, return_minutes_data
 
 
 def part_1(input_data):
-    input_data_sorted = sort_input(input_data)
+    input_data = sorted(input_data)
+    minutes_of_each_guard, minutes_data = get_minutes_of_each_guard(input_data)
+
+    current_sleep_duration_max = 0
+    guard = None
+    for key, sleep_durations in minutes_of_each_guard.items():
+        sleep_duration_max = max(sleep_durations)
+
+        if sleep_duration_max > current_sleep_duration_max:
+            current_sleep_duration_max = sleep_duration_max
+            guard = key
+
+    current_most_frequent_minute = 0
+    minute = None
+    for index, minute_data in enumerate(minutes_data):
+        frequent_minute = minute_data.count(guard)
+        if frequent_minute > current_most_frequent_minute:
+            current_most_frequent_minute = frequent_minute
+            minute = index
+
+    print("Guard {} sleeps most frequent on minute {} and the solution is {}".format(guard, minute, guard * minute))
 
 
 if __name__ == "__main__":
